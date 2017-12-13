96a3c6a919e976c74d73becd77a21b4843c8c7ec
Elton Santos <eltonaxl@hotmail.com>
2017-12-13 19:50:57 -0300

    Consertand bug no script do sorteio de vários nomes

--- sorteio_varios_nomes/sorteio.sh
+++ sorteio_varios_nomes/sorteio.sh
@@ -10,7 +10,7 @@ for dia in Segunda Terca Quarta Quinta Sexta Sabado
 do
 	qtd=`wc -l $nomes |cut -d" " -f1`
 	num=$(((RANDOM%$qtd) +1))
-	nome=`nl $nomes |egrep "\b%num\b"`
+	nome=`nl $nomes |egrep "\b$num\b"`
 	nomesorteado=`echo $nome | awk -F" " '{ print $2" "$3 }'`
 
 	echo -n "$dia : `echo $nome | awk -F' ' '{ print $2" "$3 }' `"
